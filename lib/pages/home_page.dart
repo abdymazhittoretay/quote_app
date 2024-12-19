5ca4fecff4da9adb9b8b23d799d06003bac90203
Toretay <abdymvzhit@gmail.com>
2024-12-19 21:23:57 +0500

    added http dep and opened access to Quote API

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future getQuotes() async {
+    final response = await http.get(Uri.https("zenquotes.io", "/api/quotes"));
+    final data = jsonDecode(response.body);
+    print(data);
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    getQuotes();
     return Scaffold(
       backgroundColor: Colors.white,
     );
